1a62183bed110591a14e4456b4979bf09f440c7a
sonukumar81800 <sonu.patna0808@gmail.com>
2022-05-27 14:51:09 +0530

    Leetcode solution 566. Reshape the Matrix

---
+++ Leetcode/Solution566.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Solution566 {
+  public static void main(String[] args) {
+    int[][] mat = { { 1, 2 }, { 3, 4 } };
+    int r = 1, c = 4;
+    int[][] res = matrixReshape(mat, r, c);
+    System.out.println(Arrays.deepToString(res));
+  }
+
+  public static int[][] matrixReshape(int[][] mat, int r, int c) {
+    if (r * c != mat.length * mat[0].length)
+      return mat;
+    int[][] res = new int[r][c];
+    int row = 0, col = 0;
+    for (int i = 0; i < mat.length; i++) {
+      for (int j = 0; j < mat[0].length; j++) {
+        res[row][col] = mat[i][j];
+        if (col < c) {
+          col++;
+        } else {
+          col = 0;
+          row++;
+        }
+      }
+
+    }
+    return res;
+  }
+
+}
