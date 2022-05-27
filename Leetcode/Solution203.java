1529817fa3238ff6cd0fe43a023d9a0d6afdf1eb
sonukumar81800 <sonu.patna0808@gmail.com>
2022-05-28 01:35:19 +0530

    Leetcode solution 203. Remove Linked List Elements

---
+++ Leetcode/Solution203.java
@@ -0,0 +1,54 @@
+/**
+ * Sonu Kumar (https://github.com/SonuKumar81800)
+ */
+public class Solution203 {
+  public static void main(String[] args) {
+    ListNode head = new ListNode(1, new ListNode(2,
+        new ListNode(6, new ListNode(3, new ListNode(4,
+            new ListNode(5, new ListNode(6)))))));
+    ListNode res = removeElements(head, 6);
+    while (res != null) {
+      System.out.print(res.val);
+      if (res.next != null) {
+        System.out.print(" -> ");
+      }
+      res = res.next;
+    }
+  }
+
+  public static ListNode removeElements(ListNode head, int val) {
+    ListNode res = head;
+
+    while (head != null) {
+      if (head.val == val)
+        res = head.next;
+      if (head.next != null && head.next.val == val) {
+        if (head.next.next != null) {
+          head.next = head.next.next;
+        } else
+          head.next = null;
+      } else {
+        head = head.next;
+
+      }
+    }
+    return res;
+  }
+}
+
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {
+  }
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
